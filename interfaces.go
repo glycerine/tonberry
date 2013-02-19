26e5b13502aadbadd1061d0ae36b2c7cbbf9fc1c
zeroshade <zotthewizard@gmail.com>
2013-02-19 00:32:07 -0500

    Adding constants to copy the sdl ones, adding gameobject and gamestate interfaces and some sprite modifications.

---
+++ interfaces.go
@@ -0,0 +1,18 @@
+package tonberry
+
+type GameState interface {
+	Init()
+	Clean()
+	Pause()
+	Resume()
+	HandleEvents(Event, Game)
+	Update(uint32, Game)
+	Draw(Game)
+}
+
+type GameObject interface {
+	Update(deltaTicks uint32)
+	ResetMovement()
+	HandleInput(KeyboardEvent)
+	Draw(Screen)
+}
